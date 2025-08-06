ef874f9acdbd10ce5e25c2e13c3be68cfe38c8fa
PONSINTHU-01 <ponsindhu01@gmail.com>
2025-08-06 14:15:52 +0530

    Add variables.py

---
+++ variables.py
@@ -0,0 +1,49 @@
+# name = "Geroge"  
+# age = 22 
+
+# print(name)
+# print(age)
+# print(type(age))
+
+# x=["apple","banana","cherry"]
+# print(type(x))
+
+# for y in x:
+#    print(y)
+
+#  x=str("Hello world")
+#  x=int(20)
+#  x=float(20.5)
+
+#  x=8
+#  print(x>3 and x<10)
+
+# y=5
+# y+=3
+# print(y)
+
+# fruits = ["apple", "banana", "cherry"]
+# for a in fruits:
+#   print(a)
+
+# for b in "banana":
+#   print(b)
+
+# for i in range(5):
+#     print(i)
+
+name=["ram","mahesh","prabhu","dhanu","krish"]
+# for x in name:
+#     if(x=="prabhu"):
+#         print(x)
+
+for x in name:
+    print(x)
+    if(x=="dhanu"):
+        print(x)        
+
+# for x in name:
+#   print(x)
+#   if(x=="ram"):
+#     break
+#   print(x)
